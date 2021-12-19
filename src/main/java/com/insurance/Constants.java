2f9bfac9bd73f63aa8e75147ca4ac5aad857542b
Lucas <61554619+Heyimlulu@users.noreply.github.com>
2021-12-19 16:46:59 +0100

    rename project files

--- src/main/java/com/insurance/Constants.java
+++
@@ -1,5 +0,0 @@
-package com.insurance;
-
-public class Constants {
-	public static final int PAGE_SIZE = 5;
-}
---
+++ src/main/java/com/semainier/SemainierApplication.java
@@ -0,0 +1,13 @@
+package com.semainier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemainierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemainierApplication.class, args);
+	}
+
+}
