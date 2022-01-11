e6751bebd103efaad0efec8670dfa2aa0214446c
Lucas <61554619+Heyimlulu@users.noreply.github.com>
2022-01-11 15:03:21 +0100

    Add PDF export

--- src/main/java/com/semainier/controller/WorkController.java
+++ src/main/java/com/semainier/controller/WorkController.java
@@ -1,9 +1,14 @@
 package com.semainier.controller;
 
+import com.itextpdf.html2pdf.ConverterProperties;
+import com.itextpdf.html2pdf.HtmlConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,9 +19,25 @@ import com.semainier.Constants;
 import com.semainier.model.Work;
 import com.semainier.repository.StudentRepository;
 import com.semainier.repository.WorkRepository;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayOutputStream;
 
 @Controller
 public class WorkController {
+
+	@Autowired
+	ServletContext servletContext;
+
+	private final TemplateEngine templateEngine;
+
+	public WorkController(TemplateEngine templateEngine) {
+		this.templateEngine = templateEngine;
+	}
 
 	@Autowired
 	private WorkRepository workRepository;
@@ -30,14 +51,39 @@ public class WorkController {
 	}
 
 	@GetMapping("/report/pdf")
-	public String showReportInPDF(Model model){
+	public ResponseEntity<byte[]> showReportInPDF(HttpServletRequest request, HttpServletResponse response, Model model){
 		Sort sort = Sort.by("id");
 		Page<Work> page = workRepository.findAll("", PageRequest.of(0, Constants.PAGE_SIZE, sort));
 
 		model.addAttribute("listWorks", page.getContent());
 		model.addAttribute("listStudent", studentRepository.findAll());
 
-		return "report";
+		// Create HTML using Thymeleaf template Engine
+		WebContext context = new WebContext(request, response, servletContext);
+		context.setVariable("listWorks", page.getContent());
+		context.setVariable("listStudent", studentRepository.findAll());
+		String reportHtml = templateEngine.process("report", context);
+
+		// Setup Source and target I/O streams
+		ByteArrayOutputStream target = new ByteArrayOutputStream();
+		ConverterProperties converterProperties = new ConverterProperties();
+		converterProperties.setBaseUri("http://localhost:8888");
+		// Call convert method
+		HtmlConverter.convertToPdf(reportHtml, target, converterProperties);
+
+		// extract output as bytes
+		byte[] bytes = target.toByteArray();
+
+
+		// Send the response as downloadable PDF
+		return ResponseEntity.ok()
+				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=report.pdf")
+				.contentType(MediaType.APPLICATION_PDF)
+				.body(bytes);
+
+
+		// Return webpage named report.html
+		//return "report";
 	}
 
 	@GetMapping("/work/page/{pageNumber}")
