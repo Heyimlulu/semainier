362f8580deaeff1e5669f292e04c15113c950057
Lucas <61554619+Heyimlulu@users.noreply.github.com>
2022-01-12 21:29:40 +0100

    Test Insert query

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,13 +8,17 @@
 --IGNORE 1 ROWS
 --(ville, npa, canton);
 
-COPY tbllocalite(ville, npa, canton)
-FROM 'src/main/resources/commune.csv'
-DELIMITER ','
-CSV HEADER;
+--COPY tbllocalite(ville, npa, canton)
+--FROM 'src/main/resources/commune.csv'
+--DELIMITER ','
+--CSV HEADER;
 
-DELETE
-FROM tbllocalite
-WHERE id IN (SELECT id
-             FROM (SELECT id, ROW_NUMBER() OVER (PARTITION BY ville ORDER BY id) AS rnum FROM tbllocalite) t
-             WHERE t.rnum > 1);
+
+
+--DELETE
+--FROM tbllocalite
+--WHERE id IN (SELECT id
+             --FROM (SELECT id, ROW_NUMBER() OVER (PARTITION BY ville ORDER BY id) AS rnum FROM tbllocalite) t
+             --WHERE t.rnum > 1);
+
+INSERT INTO tbllocalite (ville, npa, canton) VALUES ('Collombey', 1868, 'VS');
